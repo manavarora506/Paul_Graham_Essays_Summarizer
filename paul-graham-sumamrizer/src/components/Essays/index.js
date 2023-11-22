63b9c5c3710b211509c54878b75d0d679d882850
Manav Arora <manavarora506@utexas.edu>
2023-11-21 21:41:53 -0600

    basic notion connection set up

--- paul-graham-sumamrizer/src/components/Essays/index.js
+++ paul-graham-sumamrizer/src/components/Essays/index.js
@@ -1,33 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
-function Essay({ title, link, notes }) {
+function Essay({ title, date, body }) {
   return (
     <div className="px-2 md:px-8 py-4" id={title.replaceAll(" ", "-")}>
-      {/* Title and Tag */}
+      {/* Title */}
       <div className="flex">
         <p className="text-3xl font-semibold max-w-max dark:text-gray-200">
           {title}
         </p>
       </div>
 
-      {/* Notes */}
+      {/* Date */}
       <div className="mb-2 overflow-hidden">
-        <p className="text-xl py-1 dark:text-gray-300">{notes}</p>
+        <p className="text-xl py-1 dark:text-gray-300">{date}</p>
       </div>
 
-      {/* Link */}
-      <div className="mt-2 flex flex-wrap">
-        <Link href={link}>
-          <a
-            className="bg-off-white border border-off-black hover:bg-off-black hover:text-off-white font-medium text-sm py-2 px-3 mr-2 rounded text-center dark:bg-off-black dark:text-off-white dark:hover:bg-off-white dark:border-off-white dark:hover:text-off-black"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Visit
-          </a>
-        </Link>
-        
+      {/* Body */}
+      <div className="mb-2 overflow-hidden">
+        <p className="text-xl py-1 dark:text-gray-300">{body}</p>
       </div>
     </div>
   );
@@ -41,19 +32,36 @@ function Essays() {
       try {
         const response = await fetch("/.netlify/functions/fetchEssays");
         const data = await response.json();
+        console.log(data)
 
-        const structuredData = data.results.map((page) => ({
-          title: page.properties.Name.title[0].text.content,
-          link: page.properties.Link.url,
-          notes:
-            page.properties.my_notes &&
-            page.properties.my_notes.rich_text[0] &&
-            page.properties.my_notes.rich_text[0].text
-              ? page.properties.my_notes.rich_text[0].text.content
-              : "",
-        }));
+        const structuredData = data.results.map((page) => {
+          // Check and extract title
+          const title = page.properties.Title 
+            && Array.isArray(page.properties.Title.title) 
+            && page.properties.Title.title[0] 
+            && page.properties.Title.title[0].text
+            ? page.properties.Title.title[0].text.content
+            : "";
+        
+          // Check and extract date
+          const date = page.properties.Date 
+            && Array.isArray(page.properties.Date.rich_text) 
+            && page.properties.Date.rich_text[0] 
+            && page.properties.Date.rich_text[0].text
+            ? page.properties.Date.rich_text[0].text.content
+            : "";
+        
+          // Check and extract body
+          const body = page.properties.Body 
+            && Array.isArray(page.properties.Body.rich_text)
+            ? page.properties.Body.rich_text.map(text => text.text ? text.text.content : "").join(" ")
+            : "";
+        
+          return { title, date, body };
+        });
 
         setEssays(structuredData);
+        console.log(structuredData)
       } catch (e) {
         console.error(e);
       }
@@ -62,24 +70,21 @@ function Essays() {
     fetchData();
   }, []);
 
-  //
   return (
     <div className="bg-dark-gray p-8">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 overflow-y-auto max-h-[80vh]">
-        {essays.map((essay, index) => {
-            return (
-              <div
-                key={index}
-                className="bg-gray-950 p-6 rounded-xl shadow-md hover:shadow-lg transition-all duration-300"
-              >
-                <Essay
-                  title={essay.title}
-                  link={essay.link}
-                  notes={essay.notes}
-                />
-              </div>
-            );
-        })}
+        {essays.map((essay, index) => (
+          <div
+            key={index}
+            className="bg-gray-950 p-6 rounded-xl shadow-md hover:shadow-lg transition-all duration-300"
+          >
+            <Essay
+              title={essay.title}
+              date={essay.date}
+              body={essay.body}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
